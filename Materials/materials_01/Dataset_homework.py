12f36c917a1e73d8ecc964232a16280349aa0c4b
Diana_Peres <diana_peres@epam.com>
2024-01-23 09:56:39 +0100

    hw

---
+++ Materials/materials_01/Dataset_homework.py
@@ -0,0 +1,19 @@
+"""
+Content:
+
+This dataset contains around 210k news headlines.
+Each record in the dataset consists of the following attributes:
+
+'category': category in which the article was published.
+'headline': the headline of the news article.
+'authors': list of authors who contributed to the article.
+'link': link to the original news article.
+'short_description': Abstract of the news article.
+'date': publication date of the article.
+
+Task:
+
+1) Create script to pars json file into DataFrame.
+2) Clean text in 'headline' and 'short_description columns.
+
+"""
